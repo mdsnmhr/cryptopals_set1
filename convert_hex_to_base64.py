40992cb0c279aff691f359ff4a06d50a077f4abc
mdsnmhr <159313633+mdsnmhr@users.noreply.github.com>
2026-07-14 10:36:12 -0700

    Import base64 b16decode and b64encode and return successful conversion

--- convert_hex_to_base64.py
+++ convert_hex_to_base64.py
@@ -3,13 +3,17 @@
 # Author: Madison H.
 # Last Updated: 7/14/2026
 
+from base64 import b16decode, b64encode
+
 def convert_hex_to_base64(hex_input):
-    return bytes.fromhex(hex_input).decode()
+    data_raw = b16decode(hex_input, casefold=True)
+    data_b64 = b64encode(data_raw)
+    return data_b64
 
 def main():
-    hex_input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    hex_input = b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
     base64_output = convert_hex_to_base64(hex_input)
-    expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    expected = b"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
     
     if base64_output == expected:
         print(f"Input: {hex_input}")
