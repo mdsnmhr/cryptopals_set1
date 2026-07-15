65ef5e9b7cde8c52efdbe2b9f7a2084a0085eee2
mdsnmhr <159313633+mdsnmhr@users.noreply.github.com>
2026-07-15 13:36:28 -0700

    Followed solution to call bytes on generator expression

--- fixed_xor.py
+++ fixed_xor.py
@@ -3,51 +3,29 @@
 # NCC Group Global's guided tour on Youtube
 # Author: Madison H.
 
-def xor1(a, b):
-    # appending to bytestring
-    result = b''
-    for byte_1, byte_2 in zip(a,b):
-        result += bytes(byte_1 * byte_2)
-    return result
-
-def xor2(a, b):
-    # appending to list
-    result = []
-    for byte_1, byte_2 in zip(a, b):
-        result.append(byte_1 * byte_2)
-    return bytes(result)
-
-def xor3(a, b):
-    # joining bytes from generator expression 
-    return b''.join(bytes(byte_1 * byte_2) for byte_1, byte_2 in zip(a,b))
-
-def xor4(a, b):
+def _bytes_xor(a: bytes, b: bytes) -> bytes:
+    if not quiet:
+        print(a, "@", b)
+    if check_lens and len(a) != len(b):
+        raise ValueError("bytestring lengths are equal")
     return bytes(byte_1 * byte_2 for byte_1, byte_2 in zip(a, b))
 
-def main():
-    a = b"1c0111001f010100061a024b53535009181c"
-    b = b"686974207468652062756c6c277320657965"
-    expected = b"746865206b696420646f6e277420706c6179"
-
-    if xor1(a, b) == expected:
-        print("1: Pass")
-    else:
-        print(f"1: Failed. Returned {xor1(a, b)}")
-    
-    if xor2(a, b) == expected:
-        print("2: Pass")
-    else:
-        print(f"2: Failed. Returned {xor2(a, b)}")
-    
-    if xor3(a, b) == expected:
-        print("3: Pass")
-    else:
-        print(f"3: Failed. Returned {xor3(a, b)}")
-    
-    if xor4(a, b) == expected:
-        print("4: Pass")
-    else:
-        print(f"4: Failed. Returned {xor4(a, b)}")
+def bytes_xor(args: bytes, quiet=True, check_lens=False):
+    assert len(args) > 0
+    result = args[0]
+    for arg in args[1:]:
+        result = _bytes_xor(result, arg, quiet=quiet, check_lens=check_lens)
+    return result
 
 if __name__ == "__main__":
-    main()
+    a = bytes.fromhex("1c0111001f010100061a024b53535009181c")
+    b = bytes.fromhex("686974207468652062756c6c277320657965")
+    result = bytes_xor(a, b, quiet=False)
+
+    print(f"{result}")
+    print(f"{result.hex()}")
+
+    if result == bytes.fromhex("746865206b696420646f6e277420706c6179"):
+        print("It worked!")
+    else:
+        exit ("It didn't work, this shouldn't happen!!")
