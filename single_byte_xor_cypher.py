2edd5afd9bb6f610cf9ff4a1e18d04c69808331a
mdsnmhr <159313633+mdsnmhr@users.noreply.github.com>
2026-07-17 10:16:53 -0700

    Testing first solution

--- single_byte_xor_cypher.py
+++ single_byte_xor_cypher.py
@@ -6,6 +6,8 @@
 
 from collections import Counter
 from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from fixed_xor import bytes_xor
+from pprint import pprint
 
 # step one: find a file w/ plain text to allow us to do frequency analysis
 # frequency analysis is used to evaluate chracter frequency and decide the 
@@ -19,6 +21,40 @@ def get_freq(text, letters):
     for letter in letters:
         counts[letter] += text.count(letter)
     total = sum(counts.values())
-    return (letter: counts[letters] / total for letter in letters)
+    
+    return {letter: counts[letters] / total for letter in letters}
 
-get_freq(book, ascii_lowercase)
+frequencies = get_freq(book, ascii_letters)
+
+# score text based on frequencies
+def score_text(text: bytes) -> float:
+    score = 0.0
+    l = len(test)
+    
+    for letter, freq_expected in frequencies.items():
+        freq_actual = text.count(ord(letter)) / l
+        err = abs(freq_expected - freq_actual)
+        score += err
+    
+    return score
+
+# simple solution for cracking xor cypher
+def crack_xor_cypher(cypher: bytes) -> tuple[int, bytes]:
+    best_guess = (float['inf'], None)
+    
+    for candidate_key in range(256):
+        full_key = bytes([candidate_key]) + len(cypher)
+        plain_text = bytes_xor(full_key, cypher)
+        score = score_text(plain_text)
+        curr_guess = (score, plain_text)
+        best_guess = min(best_guess, curr_guess)
+    
+    if best_guess[1] is None:
+        exit("No key found [this should never happen]")
+    
+    return best_guess
+
+
+cypher_text = bytes,fromhex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+pprint(crack_xor_cypher(cypher_text))
+        
